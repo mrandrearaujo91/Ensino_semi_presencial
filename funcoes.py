fcc5a152867d227dfdc1d3f2fd8989e93fd81e10
mrandrearaujo91 <168090907+mrandrearaujo91@users.noreply.github.com>
2024-09-28 15:24:47 +0000

    salvar

---
+++ funcoes.py
@@ -0,0 +1,6 @@
+def nomeCompleto(primeiroNome,segundoNome):
+    print(f"O Seu nome completo é: {primeiroNome} {segundoNome}")
+
+primeiroNome=input("Digite o Seu Nome: ")    
+segundoNome=input("Digite o Seu Sobrenome: ")
+nomecompleto=nomeCompleto(primeiroNome,segundoNome)
