e7cc8fd64ca7ddfa3b5562e45c88c1f0dd8b6437
mrandrearaujo91 <168090907+mrandrearaujo91@users.noreply.github.com>
2024-10-05 12:30:41 +0000

    salvar

--- funcoes.py
+++ funcoes.py
@@ -1,6 +1,13 @@
+import os
+os.system("cls||clear")
 def nomeCompleto(primeiroNome,segundoNome):
     print(f"O Seu nome completo é: {primeiroNome} {segundoNome}")
 
-primeiroNome=input("Digite o Seu Nome: ")    
+os.system("cls||clear")
+primeiroNome=input("Digite o Seu Nome: ")
+
+os.system("cls||clear")
 segundoNome=input("Digite o Seu Sobrenome: ")
+
+os.system("cls||clear")
 nomecompleto=nomeCompleto(primeiroNome,segundoNome)
