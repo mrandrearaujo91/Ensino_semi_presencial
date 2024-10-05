7b642819107036906611cf245c3fc50cc0219212
mrandrearaujo91 <168090907+mrandrearaujo91@users.noreply.github.com>
2024-10-05 14:19:38 +0000

    salvar

--- interfaces/moedas.py
+++ interfaces/moedas.py
@@ -1,1 +1,21 @@
 import customtkinter as ctk
+ctk.set_appearance_mode('dark')
+
+janela = ctk.CTk()
+janela.geometry('400 x 250')
+janela.resizable(False,False)
+
+ctk.CTkLabel(janela,
+             text="Sistema de Conversão"#tema que vai aparecer na tela
+             text_color="yelloy"#cor da letra
+             font=("arial",25,"bold")#tipo da letra, tamanho, borda ou negrito
+             ).pack()
+valor = ctk.CTkEntry(janela,
+                     width=350,
+                     height=45
+                     )
+valor.pack()
+
+
+
+janela.mainloop()
